ca4e524b490c3e6f4bda7eeb63a233a2ae14d33f
Chakima Micheau <chamicheau@gmail.com>
2017-07-24 15:56:50 +0000

    added html to index

---
+++ js/myScript.js
@@ -0,0 +1,14 @@
+$(function () {
+   $('#modal').modal('toggle')
+});
+
+ <div class="modal" id='modal'>
+     <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h3 id="myModalLabel">Error:</h3>
+        </div>
+        <div class="modal-body">
+        <p>Please correct the following errors:</p>
+        </div>
+     </div>
+ </div>
